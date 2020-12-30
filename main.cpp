eb5cb6bfdb70b092ef8d1b105af11478541e6292
R. van Elst <vanelst@dejongduke.nl>
2020-12-30 23:19:50 +0100

    Add license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+/**
+# Copyright 2020 - Remy van Elst - https://raymii.org/s/software/Cpp_exercise_in_parsing_json_http_apis_and_time_stuff.html
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Afferro General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "httplib.hpp"
 #include "json.hpp"
 
